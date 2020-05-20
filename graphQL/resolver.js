d70b14c590cda6ed590cb16fcd58ae97ec4dc1d4
Prithvi0 <prithviy96@gmail.com>
2020-05-21 03:32:27 +0530

    Modified resolver file by including user registration

--- graphQL/resolver.js
+++ graphQL/resolver.js
@@ -1,8 +1,13 @@
 const message = require('../graphQL/query').message;
+const register = require('../graphQL/mutation/users').register;
 
 // resolvers
 exports.resolvers = {
     Query: {
         message
+    },
+
+    Mutation: {
+        register
     }
 }
