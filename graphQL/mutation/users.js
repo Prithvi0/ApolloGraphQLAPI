1b1304481bc90939ef271b90038ab0177981032b
Prithvi0 <prithviy96@gmail.com>
2020-05-21 23:53:35 +0530

    Modified mutation file by adding user input validations

--- graphQL/mutation/users.js
+++ graphQL/mutation/users.js
@@ -9,13 +9,27 @@
 const userModel = require('../../model/user');
 
 exports.register = async (parent, args, context) => {
-    // validate email
+    // validate user name
+    if (args.userName === null) {
+        throw new Error('User name can not be blank.')
+    }
+    if (args.userName.length < 3) {
+        throw new Error('Minimum of 3 characters required.')
+    }    
+
+    // validate e-mail
     const emailRegex = /^[a-zA-Z]+([+-_.][a-zA-Z0-9])*[0-9]*\@[a-z0-9]+[.]([a-z]{2,4}[.])?[a-z]{2,4}$/;
+    if (args.emailId === null) {
+        throw new Error('E-mail can not be blank. Please try entering a valid e-mail.');
+    }
     if (!emailRegex.test(args.emailId)) {
         throw new Error('Invalid e-mail');
     }
 
     // validate password
+    if (args.password === null) {
+        throw new Error('Password can not be blank. Please Enter a 8-character long password');
+    }
     if (args.password.length < 8) {
         throw new Error('password must be atleast 8 characters long');
     }
