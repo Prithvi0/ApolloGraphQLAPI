84d80fc7f19a3e7b1a6957251876b10fafbf19ca
Prithvi0 <prithviy96@gmail.com>
2020-05-25 06:08:11 +0530

    Modified server file by revoking generated token

--- server.js
+++ server.js
@@ -12,12 +12,29 @@ const { typeDefs } = require('./graphQL/schema');
 
 const resolvers = require('./graphQL/resolver').resolvers;
 
+// const getAllUsers = require('./graphQL/query').getAllUsers;
+
 // The ApolloServer constructor with parameters: schema definition & set of resolvers
-module.exports = server = new ApolloServer({ typeDefs, resolvers });
+module.exports = server = new ApolloServer({
+    typeDefs, resolvers,
+    context: ({ req }) => ({
+//         const token = req.query.token || '';
+
+//    // retrieve a user with the token
+//    const user = getAllUsers(token);
+
+//    if (!user) throw new AuthenticationError('you must be logged in'); 
+
+//    // add the user to the context
+//    return { user };
+//     }
+token: req.query.token
+    })
+});
 
 // The `listen` method launches a web server
 server.listen(port, err => {
-  if (err)
-    throw new Error(err);
-  console.log(`🚀 Server ready at ${port}`);
+    if (err)
+        throw new Error(err);
+    console.log(`🚀 Server ready at ${port}`);
 });
