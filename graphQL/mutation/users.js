4769228e8402892f28f58a1e11e329b54be7fa08
Prithvi0 <prithviy96@gmail.com>
2020-05-25 09:07:25 +0530

    Modified mutation file by adding url-shortener

--- graphQL/mutation/users.js
+++ graphQL/mutation/users.js
@@ -157,3 +157,28 @@ module.exports = {
       }
     }
 }
+
+// url-shortener
+let request = require("request");
+let linkRequest = {
+  destination: "https://www.youtube.com/channel/UCHK4HD0ltu1-I212icLPt3g",
+  domain: { fullName: "rebrand.ly" }
+  //, slashtag: "A_NEW_SLASHTAG"
+  //, title: "Rebrandly YouTube channel"
+}
+
+let requestHeaders = {
+  "Content-Type": "application/json",
+  "apikey": "YOUR_API_KEY",
+  "workspace": "YOUR_WORKSPACE_ID"
+}
+
+request({
+    uri: "https://api.rebrandly.com/v1/links",
+    method: "POST",
+    body: JSON.stringify(linkRequest),
+    headers: requestHeaders
+}, (err, response, body) => {
+  let link = JSON.parse(body);
+  console.log(`Long URL was ${link.destination}, short URL is ${link.shortUrl}`);
+})
