669e0e256b23c651a08f1c98dec07b4d014e9a95
Prithvi0 <prithviy96@gmail.com>
2020-05-26 09:13:18 +0530

    Modified file by replacing query with authorization

--- server.js
+++ server.js
@@ -12,23 +12,11 @@ const { typeDefs } = require('./graphQL/schema');
 
 const resolvers = require('./graphQL/resolver').resolvers;
 
-// const getAllUsers = require('./graphQL/query').getAllUsers;
-
 // The ApolloServer constructor with parameters: schema definition & set of resolvers
 module.exports = server = new ApolloServer({
     typeDefs, resolvers,
     context: ({ req }) => ({
-//         const token = req.query.token || '';
-
-//    // retrieve a user with the token
-//    const user = getAllUsers(token);
-
-//    if (!user) throw new AuthenticationError('you must be logged in'); 
-
-//    // add the user to the context
-//    return { user };
-//     }
-token: req.query.token
+        authorization: req.headers.authorization
     })
 });
 
