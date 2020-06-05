13f1f4aa87da7c635e8c75adeea46c777b3d440a
Prithvi0 <prithviy96@gmail.com>
2020-06-05 23:59:54 +0530

    Added function to get access token

--- graphQL/mutation/gitHubAuthorization.js
+++ graphQL/mutation/gitHubAuthorization.js
@@ -3,10 +3,46 @@
  * Mutations are defined as a part of the schema.
  */
 
+// Module imports
+const clientId = process.env.CLIENT_ID;
+const axios = require('axios');
+const clientSecret = process.env.CLIENT_SECRET;
+
 /** It is used to return user code associated with the GitHub.
  * @function (githubLoginUrl)
  * @returns - user login authorization for GitHub.
  */
 exports.githubLoginUrl = () => {
-    return `${process.env.LOGIN_AUTH}${process.env.CLIENT_ID}&scope=user`
+    let githubLoginUrl = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&scope=user';
+    return githubLoginUrl;
+}
+
+/** It is used to request a GitHub access token with credentials.
+ * The credentials are sent to a GitHub API URL in the body of a POST request.
+ * The credentials consist of the client_id, client_secret, and code.
+ * Once completed, the GitHub response is then parsed.
+ * @function (requestGithubToken)
+ * @constructor                 - parent is required.
+ * @param {String} args         - args for code (get GitHub access token).
+ * @param {String} tokenRequest - to access information from the GitHub user's account.
+ */
+exports.requestGithubToken = (parent, args, context) => {
+    let tokenRequest = 'https://github.com/login/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + `&code=${args.code}`;
+    let post = axios.post(tokenRequest)
+        .then(tokenRequest => {
+            console.log(tokenRequest)
+            if (post) {
+                return ({
+                    message: 'Access token for login has been generated',
+                    success: true,
+                    token: tokenRequest
+                });
+            }
+        }).catch(error => {
+            throw new Error({
+                message: 'No Access token generated',
+                success: false,
+                token: error
+            });
+        })
 }
